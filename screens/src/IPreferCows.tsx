429b464d76d2839f22558f9ceddb0839fbbe26d9
Tanguy Antoine <antoine.tanguy@apizi.com>
2018-05-30 15:32:39 +0200

    Config + fetcher

--- screens/src/IPreferCows.tsx
+++ screens/src/IPreferCows.tsx
@@ -1,15 +1,29 @@
 import { Component } from '@stencil/core'
 import '@apizi/ui'
+import { BearerComponent, Intent, BearerFetch, State } from '@apizi/core'
 
+@BearerComponent
 @Component({
   tag: 'i-prefer-cows',
   styleUrl: 'IPreferCows.css',
   shadow: true
 })
 export class IPreferCows {
+  @Intent('cowList') getCows: BearerFetch
+  @State() cows: Array<string> = []
+
+  componentDidLoad() {
+    this.getCows().then(({ items }) => {
+      this.cows = items
+    })
+  }
+
   render() {
     return (
-      <div>IPreferCows component </div>
+      <div>
+        IPreferCows component
+        <ul>{this.cows.map(cow => <li>{cow}</li>)}</ul>
+      </div>
     )
   }
 }
