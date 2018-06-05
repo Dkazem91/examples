25e4fcce13d99c40d36e1b0ecde34ba47aaf980c
Tanguy Antoine <antoine.tanguy@apizi.com>
2018-06-05 13:17:00 +0200

    Use scroll paginator

--- AttachPullRequest/intents/listPullRequests.js
+++ AttachPullRequest/intents/listPullRequests.js
@@ -1,33 +1,33 @@
-const { GetCollection } = require("@apizi/intents");
+const { GetCollection } = require('@apizi/intents')
 
-const axios = require("axios");
+const axios = require('axios')
 
 const CLIENT = axios.create({
-  baseURL: "https://api.github.com/",
+  baseURL: 'https://api.github.com/',
   timeout: 5000,
   headers: {
-    Accept: "application/vnd.github.v3+json",
-    "User-Agent": "Bearer"
+    Accept: 'application/vnd.github.v3+json',
+    'User-Agent': 'Bearer'
   }
-});
+})
 
 function headersFor(token) {
   return {
     Authorization: `token ${token}`
-  };
+  }
 }
 
 module.exports.action = (token, { fullName, ...params }, callback) =>
   CLIENT.get(`/repos/${fullName}/pulls`, {
-    params,
+    params: { ...params, per_page: 10 },
     headers: headersFor(token)
   })
     .then(response => {
-      callback({ collection: response.data });
+      callback({ collection: response.data })
     })
     .catch(e => {
-      console.log(e);
-      callback({ collection: [] });
-    });
-module.exports.intentType = GetCollection;
-module.exports.intentName = "listPullRequests";
+      console.log(e)
+      callback({ collection: [] })
+    })
+module.exports.intentType = GetCollection
+module.exports.intentName = 'listPullRequests'
