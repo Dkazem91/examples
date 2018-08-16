98747932862475252c39fdf9edf94d8dbadf4494
Cédric Fabianski <cfabianski@me.com>
2018-08-16 15:53:47 +0200

    Remove config.dev.js from source

--- githubAttachPullRequests/config.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  global: { authAccess: { accessToken: '4a20e2b48fd146fb111e72c0162c4d5e8cb9aa89' } },
-  // IntentName: { customField: 'Custom Value that will be added to the context' }
-}
