dc3c99b78228d0e547c75079f8481512ed87acb0
Pierre Repetto-Andipatin <pierre@bearer.sh>
2018-08-17 12:25:25 +0200

    feat: add style

--- addevent/views/components/showCalendar.tsx
+++ addevent/views/components/showCalendar.tsx
@@ -6,12 +6,20 @@ import { Component } from '@bearer/core'
   shadow: true
 })
 export class ShowCalendar {
+
+  getCurrentDate() {
+    return new Date().toISOString().slice(0, 10)
+  }
+
   render() {
     return (
       <div class="root">
-        <bearer-typography as="h2">
-          ShowCalendar Component
+        <bearer-typography as="h3">
+          Pick a date
         </bearer-typography>
+        <input type="date" name="date" class="form-control"
+          value={this.getCurrentDate()}
+          min={this.getCurrentDate()} max="2022-12-31" />
       </div>
     )
   }
